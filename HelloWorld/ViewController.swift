a048e1d8ae708533c2d0167ce1b9c650652bdbbb
Igor Solodyankin <solodyankin.ie@ya.ru>
2022-02-19 08:24:24 +0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Игорь Солодянкин on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
